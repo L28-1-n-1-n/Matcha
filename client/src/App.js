321ca0b53dbf99851c52a46896a2c556062fb58a
L28-1-n-1-n <zto2.71828@gmail.com>
2020-03-09 11:31:47 +0000

    two options for routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,111 @@
+// import React, { Fragment, useEffect } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Navbar from './components/layout/Navbar';
+// import Landing from './components/layout/Landing';
+// import Register from './components/auth/Register';
+// import Login from './components/auth/Login';
+// import Alert from './components/layout/Alert';
+// import Dashboard from './components/dashboard/Dashboard';
+// import CreateProfile from './components/profile-forms/CreateProfile';
+// import EditProfile from './components/profile-forms/EditProfile';
+// import AddExperience from './components/profile-forms/AddExperience';
+// import AddEducation from './components/profile-forms/AddEducation';
+// import Profiles from './components/profiles/Profiles';
+// import Profile from './components/profile/Profile';
+// import Posts from './components/posts/Posts';
+// import Post from './components/post/Post';
+// import NotFound from './components/layout/NotFound';
+// import PrivateRoute from './components/routing/PrivateRoute';
+// import { loadUser } from './actions/auth';
+// import setAuthToken from './utils/setAuthToken';
+// // Redux
+// import { Provider } from 'react-redux'; // connect react with redux
+// // wrap everything in <Provider></Provider> so that react-redux affects everything
+// import store from './store';
+
+// import './App.css';
+
+// if (localStorage.token) {
+//   setAuthToken(localStorage.token);
+// }
+
+// // const App = () => {
+// //   useEffect(() => {
+// //     store.dispatch(loadUser());
+// //   }, []);
+// const App = () => {
+//   useEffect(() => {
+//     if (localStorage.token) {
+//       setAuthToken(localStorage.token);
+//       store.dispatch(loadUser());
+//     }
+//   }, []);
+
+//   // empty [] added as second parameter such that it only runs once, otherwise useEffect is a constant loop when the state updates
+//   // the empry [] serve the same purpose of componentDidMount had useEffect been a class, not a function
+//   return (
+//     <Provider store={store}>
+//       <Router>
+//         <Fragment>
+//           <Navbar />
+//           <Route exact path='/' component={Landing} />
+//           <section className='container'>
+//             <Alert />
+//             {/* Switch can only have Route in it */}
+//             <Switch>
+//               <Route exact path='/register' component={Register} />
+//               <Route exact path='/login' component={Login} />
+//               <Route exact path='/profiles' component={Profiles} />
+//               <Route exact path='/profile/:id' component={Profile} />
+//               {/* Force user to login */}
+//               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+//               <PrivateRoute
+//                 exact
+//                 path='/create-profile'
+//                 component={CreateProfile}
+//               />
+//               <PrivateRoute
+//                 exact
+//                 path='/edit-profile'
+//                 component={EditProfile}
+//               />
+//               <PrivateRoute
+//                 exact
+//                 path='/add-experience'
+//                 component={AddExperience}
+//               />
+//               <PrivateRoute
+//                 exact
+//                 path='/add-education'
+//                 component={AddEducation}
+//               />
+//               <PrivateRoute exact path='/posts' component={Posts} />
+//               <PrivateRoute exact path='/posts/:id' component={Post} />
+//               <Route component={NotFound} />
+//             </Switch>
+//           </section>
+//         </Fragment>
+//       </Router>
+//     </Provider>
+//   );
+// };
+
+// export default App;
+
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/profile-forms/CreateProfile';
-import EditProfile from './components/profile-forms/EditProfile';
-import AddExperience from './components/profile-forms/AddExperience';
-import AddEducation from './components/profile-forms/AddEducation';
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import Posts from './components/posts/Posts';
-import PrivateRoute from './components/routing/PrivateRoute';
+import Routes from './components/routing/Routes';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
-// Redux
-import { Provider } from 'react-redux'; // connect react with redux
-// wrap everything in <Provider></Provider> so that react-redux affects everything
-import store from './store';
 
 import './App.css';
 
-if (localStorage.token) {
-  setAuthToken(localStorage.token);
-}
-
-// const App = () => {
-//   useEffect(() => {
-//     store.dispatch(loadUser());
-//   }, []);
 const App = () => {
   useEffect(() => {
     if (localStorage.token) {
@@ -39,47 +114,15 @@ const App = () => {
     }
   }, []);
 
-  // empty [] added as second parameter such that it only runs once, otherwise useEffect is a constant loop when the state updates
-  // the empry [] serve the same purpose of componentDidMount had useEffect been a class, not a function
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Alert />
-            {/* Switch can only have Route in it */}
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/profiles' component={Profiles} />
-              <Route exact path='/profile/:id' component={Profile} />
-              {/* Force user to login */}
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute
-                exact
-                path='/create-profile'
-                component={CreateProfile}
-              />
-              <PrivateRoute
-                exact
-                path='/edit-profile'
-                component={EditProfile}
-              />
-              <PrivateRoute
-                exact
-                path='/add-experience'
-                component={AddExperience}
-              />
-              <PrivateRoute
-                exact
-                path='/add-education'
-                component={AddEducation}
-              />
-              <PrivateRoute exact path='/posts' component={Posts} />
-            </Switch>
-          </section>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
